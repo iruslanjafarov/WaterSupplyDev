04e2b134bdb5d5310d88170143d9709f63425c6d
Ruslan Jafarov <rusyajaf@gmail.com>
2023-09-20 23:01:35 +0300

    start header

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from "vite";
+import autoprefixer from "autoprefixer";
+
+export default defineConfig({
+    base: "./",
+    build: {
+        minify: true,
+        sourcemap: true
+    },
+    server: {
+        host: "0.0.0.0",
+        port: 8888,
+        open: true,
+        hmr: true
+    },
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer({})
+            ],
+        },
+    }
+});
