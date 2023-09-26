e2cd32bb89ae24bb18ef2f7f0d526d6f9fc08fd5
Ruslan Jafarov <rusyajaf@gmail.com>
2023-09-26 16:29:44 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 import "../styles/style.scss";
+
+const burger = document.querySelector(".burger"), headerLinks = document.querySelector(".header-links");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("burger_active")
+    headerLinks.classList.toggle("header-links_active");
+});
